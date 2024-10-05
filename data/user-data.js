b6bf0a510c72b921be75fb204f1a43d4aaa87bc1
Badrusalsyihab <badrusalsyihab@gmail.com>
2024-10-05 19:20:24 +0800

    Removed due to sensitive data

---
+++ data/user-data.js
@@ -0,0 +1,13 @@
+export const userData = {
+  githubUser: 'dharam-gfx',
+  devUsername: "dharam-gfx",
+  github: 'https://github.com/dharam-gfx',
+  facebook: 'https://facebook.com/dharam01.official',
+  linkedIn: 'https://www.linkedin.com/in/dharmendra-kumar-a588a4119/',
+  twitter: 'https://twitter.com/dharam_gfx',
+  stackOverflow: 'https://stackoverflow.com/users/24954262/dharmendra-kumar',
+  leetcode: "https://leetcode.com/u/dharam-gfx/",
+  resume: "#",
+  skills: ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'Vue 3', 'React', 'NextJS', 'Redux', 'NodeJS', 'Express', 'MongoDB', 'bootstrap', 'tailwind ', 'AntDesign', 'shadcn', 'Figma', 'Adobe Photoshop', 'Premiere Pro'],
+  timezone: '+6'
+};
